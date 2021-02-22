d64c4b98cee6c20b35d95eb8fa9855f1fe822304
Aditya <aditya39vampire@gmail.com>
2021-02-22 14:15:58 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//jshint version : 6
+
+const express= require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const app = express();
+
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+
+app.listen(3000, function (params) {
+    console.log("Server started on port 3000");
+})
